58979a51c8d59b8fa14a7b918f1b59fa868b6520
Dai-Trinh <97729352+Dai-Trinh@users.noreply.github.com>
2023-11-29 23:09:35 +0700

    fix cors

--- src/main/java/com/pttkhttt/PTTKHTTT_NMH_BE/config/CorConfig.java
+++ src/main/java/com/pttkhttt/PTTKHTTT_NMH_BE/config/CorConfig.java
@@ -1,5 +1,7 @@
 package com.pttkhttt.PTTKHTTT_NMH_BE.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,14 +12,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorConfig implements WebMvcConfigurer {
 
 
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5500") // Đặt origin của frontend của bạn
-                    .allowedMethods("GET", "POST", "PUT", "DELETE")
-                    .allowedHeaders("Authorization", "Content-Type")
-                    .allowCredentials(true)
-                    .maxAge(3600);
-        }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Cho phép CORS cho tất cả các đường dẫn
+                        .allowedOrigins("*") // Cho phép tất cả các nguồn gốc
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Cho phép các phương thức
+                        .allowedHeaders("*"); // Cho phép tất cả các header
+            }
+        };
+    }
+
+
+
+
 
 }
