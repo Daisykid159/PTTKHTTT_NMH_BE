eeedfb539d2b91a0850137f151f23b721a43fa70
Dai-Trinh <97729352+Dai-Trinh@users.noreply.github.com>
2023-11-29 20:33:49 +0700

    fix login

--- src/main/java/com/pttkhttt/PTTKHTTT_NMH_BE/response/CommonResponse.java
+++ src/main/java/com/pttkhttt/PTTKHTTT_NMH_BE/response/CommonResponse.java
@@ -28,5 +28,27 @@ public class CommonResponse<T> {
         return this;
     }
 
+    public boolean isOk() {
+        return ok;
+    }
 
+    public void setOk(boolean ok) {
+        this.ok = ok;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
--- src/main/java/com/pttkhttt/PTTKHTTT_NMH_BE/restcontroller/UserResource.java
+++ src/main/java/com/pttkhttt/PTTKHTTT_NMH_BE/restcontroller/UserResource.java
@@ -1,20 +1,22 @@
-package com.pttkhttt.PTTKHTTT_NMH_BE.controller;
+package com.pttkhttt.PTTKHTTT_NMH_BE.restcontroller;
 
 import com.pttkhttt.PTTKHTTT_NMH_BE.response.CommonResponse;
 import com.pttkhttt.PTTKHTTT_NMH_BE.service.UserService;
 import com.pttkhttt.PTTKHTTT_NMH_BE.service.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-public class UserController {
+public class UserResource {
 
     @Autowired
     UserService userService;
 
-    @PostMapping("/dang-nhap")
+    @PostMapping(value = "/dang-nhap")
     public CommonResponse dangNhap(@RequestBody UserDTO userDTO){
         return userService.dangNhap(userDTO);
     }
